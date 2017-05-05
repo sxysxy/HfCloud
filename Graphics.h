d092b5f03fcc7ee29c1842261801bbe84e9705ef
sxysxy <1441157749@qq.com>
2017-05-06 00:50:15 +0800

    update readme, 去掉Graphics冗余的功能

--- Graphics.h
+++ Graphics.h
@@ -20,17 +20,6 @@ public:
     static void update();  /**<Graphics::update, very important but online has one line of code.*/
     static void clear();   /**<clear the Graphics*/
     static void set_title(const std::string &t);  /**<set the title of the window. */
-
-    static void set_render_color(HfCloud::Color color);  /**<set the color of then render when uses render's drawing functions*/
-    static void fill_rect(Hf_Rect &rect);    /**<render a rect. */
-    static void fill_rect(Hf_Rect &rect, HfCloud::Color color);
-    static void draw_line(int x1, int y1, int x2, int y2);      /**<render a line */
-    static void draw_line(int x1, int y1, int x2, int y2, HfCloud::Color color);
-    static void set_pixel(int x, int y);     /**<render a point; */
-    static void set_pixel(int x, int y, HfCloud::Color color);
-    static void fill_rects(Hf_Rect *rects, int cnt);  /**<render some rects. */
-    static void set_pixels(Hf_Point *ps, int cnt);    /**<redner some points. */
-
 };
 
 HFCLOUD_END
