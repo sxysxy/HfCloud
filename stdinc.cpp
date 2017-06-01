7718cc14d2c57abf7dfc5c29d7d8630f12375bd2
sxysxy <1441157749@qq.com>
2017-06-01 18:25:28 +0800

    解决了在mingw环境下链接出错的问题，顺便问SDL_ttf谁写的，我能不能打他一顿

--- stdinc.cpp
+++ stdinc.cpp
@@ -8,3 +8,25 @@
 #pragma comment(lib, "SDL2_ttf.lib")
 
 #endif
+
+#ifdef __MINGW32_MAJOR_VERSION
+        //mingw environment...
+        //ld : undefined refrence to 'SDL_ceil', undefined refrence to ..... WTF???
+        //then I write these functions...
+double SDLCALL SDL_ceil(double x){
+    return ceil(x);
+}
+size_t SDLCALL SDL_strlen(const char *s){
+    return strlen(s);
+}
+char *SDLCALL SDL_strchr(const char *s, int c){
+    return strchr(s, c);
+}
+size_t SDLCALL SDL_strlcpy(char *dst, const char *src, size_t maxlen){
+    for(int i = 0; i < maxlen-1 && src[i]; i++)
+        dst[i] = src[i];
+    dst[maxlen-1] = 0;
+    return strlen(src);
+}
+
+#endif
